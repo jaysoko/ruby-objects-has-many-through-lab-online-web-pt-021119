58bca263cdcc91aec075c7f8a2b7a582470f238a
Jay Sokolowski <jaysoko@gmail.com>
2019-03-26 00:49:11 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,21 @@
+class Doctor
+@@all = []
+attr_accessor :name
+
+def initialize(name)
+@name = name
+@@all << self
+end
+
+def self.all
+@@all
+end
+
+def new_appointment(date,patient)
+@date = date
+@patient = patient
+end
+
+
+
+end
